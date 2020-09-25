ec0c72007029d6a70ef74293021f6da2a588380f
rahalrazika <rahalraika@gmail.com>
2020-09-25 03:41:24 +0200

    add the comment model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,11 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
     end
+
     def new 
         @post = Post.new
     end
+
     def create 
         #render plain: params[:post].inspect 
         @post = Post.new(post_param)
@@ -18,7 +20,26 @@ class PostsController < ApplicationController
         render 'new'
        end
     end 
+    def edit
+        @post = Post.find(params[:id])
+    end
 
+    def update
+        @post = Post.find(params[:id])
+        if(@post.update(post_param)) 
+            redirect_to @post
+           else 
+            render 'edit'
+           end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
+
+    end
     private def post_param
         params.require(:post).permit(:title, :body)
 
