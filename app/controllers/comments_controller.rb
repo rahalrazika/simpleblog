2ffcdf7174eef2a9691ed835b8158f45bce14d77
rahalrazika <rahalraika@gmail.com>
2020-09-25 13:03:38 +0200

    fix rubocop errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,22 @@
 class CommentsController < ApplicationController
-    http_basic_authenticate_with name: "raza", password: "1234", only: [:destroy]
+  http_basic_authenticate_with name: 'raza', password: '1234', only: [:destroy]
 
-    def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(comment_params)
-        redirect_to post_path(@post)
-    end
-    
-    def destroy
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.find(params[:id])
-        @comment.destroy
-        redirect_to post_path(@post)
-    end
-    private 
-    def comment_params
-       params.require(:comment).permit(:username, :body)
-    end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:username, :body)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,50 @@
 class PostsController < ApplicationController
-    http_basic_authenticate_with name: "raza", password: "1234", except: [:index, :show]
-    def index 
-        @posts = Post.all
+  http_basic_authenticate_with name: 'raza', password: '1234', except: %i[index show]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    @post = Post.new(post_param)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
     end
+  end
 
-    def show
-        @post = Post.find(params[:id])
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_param)
+      redirect_to @post
+    else
+      render 'edit'
     end
+  end
 
-    def new 
-        @post = Post.new
-    end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
 
-    def create 
-        #render plain: params[:post].inspect 
-        @post = Post.new(post_param)
-       if(@post.save) 
-        redirect_to @post
-       else 
-        render 'new'
-       end
-    end 
-    def edit
-        @post = Post.find(params[:id])
-    end
+    redirect_to posts_path
+  end
 
-    def update
-        @post = Post.find(params[:id])
-        if(@post.update(post_param)) 
-            redirect_to @post
-           else 
-            render 'edit'
-           end
-    end
+  private
 
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-
-        redirect_to posts_path
-
-    end
-    private def post_param
-        params.require(:post).permit(:title, :body)
-
-    end
+  def post_param
+    params.require(:post).permit(:title, :body)
+  end
 end
